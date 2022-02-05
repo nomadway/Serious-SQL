049c8ebd669bcc0d5ef47be69ce47958b8c81d34
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-02-05 01:45:02 -0700

    sql

--- week-1/week1.sql
+++ week-1/week1.sql
@@ -8,5 +8,16 @@ FROM
   SELECT country
 FROM
   dvd_rentals.country
-ORDER BY country
+ORDER BY country --by default prints countries in alphabetical order
 LIMIT 5;
+
+--SORT WTH MUPTIPLE COLUMNS-prints categories with lowest sales-
+SELECT
+  category,
+  total_sales
+FROM
+  dvd_rentals.sales_by_film_category
+ORDER BY
+  total_sales
+LIMIT
+  5;
