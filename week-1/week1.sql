bc53b8d77d439abb92698263496ebb4e8278fc88
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-02-05 23:39:41 -0700

    Update week1.sql

--- week-1/week1.sql
+++ week-1/week1.sql
@@ -32,4 +32,23 @@ ORDER BY
 LIMIT
   1;
 
+--SORTING by multiple columns and create CTE-
+DROP TABLE IF EXISTS sample_table;
+CREATE TEMP TABLE sample_table AS WITH raw_data (id, column_a, column_b) AS (
+  VALUES
+    (1, 0, 'A'),
+    (2, 0, 'B'),
+    (3, 1, 'C'),
+    (4, 1, 'D'),
+    (5, 2, 'D'),
+    (6, 3, 'D')
+)
+SELECT
+  *
+FROM
+  raw_data;
 
+SELECT
+  *
+FROM
+  sample_table;
