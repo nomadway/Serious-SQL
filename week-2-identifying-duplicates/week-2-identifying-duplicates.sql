f60d7bcd9f2bedaf28306c466092a9a9f46806fd
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-02-07 00:29:56 -0700

    Update week-2-identifying-duplicates.sql

--- week-2-identifying-duplicates/week-2-identifying-duplicates.sql
+++ week-2-identifying-duplicates/week-2-identifying-duplicates.sql
@@ -3,9 +3,31 @@ SELECT
   *
 FROM
   health.user_logs language
-LIMIT 10;
+LIMIT
+  10;
 
 --Show number of records in the table
-SELECT COUNT(*)
+SELECT
+  COUNT(*)
+FROM
+  health.user_logs;
+
+--COUNT on a few columns
+SELECT
+  measure,
+  COUNT(*) AS frequency
+FROM
+  health.user_logs
+GROUP BY
+  measure;
+
+--Show number of unique IDs
+SELECT COUNT(DISTINCT id)
 FROM health.user_logs;
 
+--Print top 20 customers by record count (those with most records/rows in the table)
+SELECT id, COUNT(*) AS row_count
+FROM health.user_logs
+GROUP BY id
+ORDER BY row_count DESC
+LIMIT 15; 
